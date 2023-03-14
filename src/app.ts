0cea8320d07c9bfdfd228de183a1470b4e484c4c
utpalsinghdev <utpalsingh915@gmail.com>
2023-03-14 19:34:28 +0530

    cors added

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,20 @@ import "dotenv/config"
 import express, { NextFunction, Request, Response } from "express";
 import createHttpError, { isHttpError } from "http-errors";
 import morgan from "morgan";
+import cors from "cors";
 import testrou from "./test/test.routes";
 import OtpRoutes from "./otp/otp.routes";
 const app = express();
 app.use(morgan("dev"))
 app.use(express.json());
 
-
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+})
+);
 
 app.use("/api/v1/test", testrou)
 app.use("/api/otp", OtpRoutes)
